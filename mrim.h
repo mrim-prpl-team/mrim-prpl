9305193e9cf9edf4988c6b9beb81a680524a8c4d
kiv.apple <kiv.apple@gmail.com>
2011-03-19 20:12:29 +0000

    New moods added

--- mrim.h
+++ mrim.h
@@ -107,7 +107,7 @@ static struct mrim_purple_status {
 } mrim_purple_statuses[] = {
 	{ PURPLE_STATUS_OFFLINE, STATUS_OFFLINE, NULL, "offline", N_("Offline"), FALSE},
 	{ PURPLE_STATUS_AVAILABLE, STATUS_ONLINE, "status_1", "status_online", N_("Available"), TRUE },
-	{ PURPLE_STATUS_AVAILABLE, STATUS_USER_DEFINED, "status_chat", "status_chat", N_("Ready to chat"), TRUE },
+	//{ PURPLE_STATUS_AVAILABLE, STATUS_USER_DEFINED, "status_chat", "status_chat", N_("Ready to chat"), TRUE },
 	{ PURPLE_STATUS_AWAY, STATUS_AWAY, "status_2", "status_away", "Away", TRUE },
 	{ PURPLE_STATUS_UNAVAILABLE, STATUS_USER_DEFINED, "status_dnd", "status_unavailable", N_("Not available"), TRUE },
 	{ PURPLE_STATUS_INVISIBLE, STATUS_FLAG_INVISIBLE, "status_3", "invisible", N_("Invisible"), TRUE }
@@ -120,16 +120,22 @@ static struct mrim_purple_mood {
 	const char *mood;
 	const char *title;
 } mrim_purple_moods[] = {
+	//{ "status_chat", "meeting", N_("Ready to chat") }, /* TODO: SegFault */
+	{ "status_4", "sick", N_("Sick") },
+	{ "status_6", "plate", N_("Plate") },
+	{ "status_8", "restroom", N_("Restroom") },
+	{ "status_15", "bathing", N_("Bathing") },
+	{ "status_16", "console", N_("Playing") },
 	{ "status_22", "working", N_("Working") },
 	{ "status_23", "sleeping", N_("Sleeping") },
-	{ "status_29", "excited", N_("Excited") },
+	{ "status_29", "happy", N_("Happy") },
 	{ "status_30", "wink", N_("Wink") },
 	{ "status_34", "sad", N_("Sad") },
 	{ "status_35", "crying", N_("Crying") },
 	{ "status_37", "angry", N_("Angry") }
 };
 
-#define MRIM_PURPLE_MOOD_COUNT ARRAY_SIZE(mrim_purple_statuses)
+#define MRIM_PURPLE_MOOD_COUNT ARRAY_SIZE(mrim_purple_moods)
 
 typedef struct
 {
