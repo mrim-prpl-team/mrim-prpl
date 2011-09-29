a42e2ec62b0909665129172140b49fa5d12ef597
Janus.Kobain.Stamp <Janus.Kobain.Stamp@gmail.com>
2011-09-29 20:25:08 +0400

    mrim2icq allow draft.

--- mrim-util.c
+++ mrim-util.c
@@ -36,7 +36,7 @@ gboolean string_is_match(gchar *string, gchar *pattern) {
 }
 
 gboolean is_valid_email(gchar *email) {
-	return string_is_match(email, "([[:alnum:]\\_]+[[:alnum:]\\-\\.\\_]+)@(mail.ru|list.ru|inbox.ru|bk.ru|corp.mail.ru)");
+	return string_is_match(email, "([[:alnum:]\\_]+[[:alnum:]\\-\\.\\_]+)@(uin.icq|mail.ru|list.ru|inbox.ru|bk.ru|corp.mail.ru)");
 }
 
 gboolean is_valid_chat(gchar *chat) {
